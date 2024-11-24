394f80a4dfb074aede8de4816c719fe303fdf28e
mojo <igormalj@gmail.com>
2024-11-24 09:34:48 +0100

    complete signup logic, connect frontend, backend and db

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,12 +1,23 @@
 import { Request, Response, NextFunction } from "express";
 import { matchedData } from "express-validator";
 import { StatusCodes } from "http-status-codes";
+import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const postSignup = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { username, email, password, confirmPassword } = matchedData(req);
+    const { username, email, password } = matchedData(req);
 
-    res.json({ username, email, password, confirmPassword });
+    const salt = await bcrypt.genSalt(12);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    await prisma.user.create({
+      data: { username, email, password: hashedPassword },
+    });
+
+    return res.status(StatusCodes.CREATED).redirect("/login");
   } catch (error) {
     return next(error);
   }
--- src/middlewares/validationMiddlewares.ts
+++ src/middlewares/validationMiddlewares.ts
@@ -1,34 +1,52 @@
 import { NextFunction, Response, Request } from "express";
 import { validationResult, Result } from "express-validator";
 import { StatusCodes } from "http-status-codes";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 // helpers
 const getErrorMessages = (result: Result): string[] => {
   return result.array().map((err) => err.msg);
 };
 
-const signupValidationMiddleware = (
+const signupValidationMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const result = validationResult(req);
-
-  if (!result.isEmpty()) {
-    const validationErrors = getErrorMessages(result);
-    return res.status(StatusCodes.BAD_REQUEST).render("pages/signup-form", {
-      actionPath: "/signup",
-      inputValues: {
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        confirmPassword: req.body.confirmPassword,
-      },
-      validationErrors,
+  try {
+    const usernameExists = await prisma.user.findUnique({
+      where: { username: req.body.username },
     });
-  }
 
-  return next();
+    const emailExists = await prisma.user.findUnique({
+      where: { email: req.body.email },
+    });
+
+    const result = validationResult(req);
+
+    if (usernameExists || emailExists || !result.isEmpty()) {
+      const validationErrors = getErrorMessages(result);
+      if (usernameExists) validationErrors.push("username already exists");
+      if (emailExists) validationErrors.push("email already in use");
+
+      return res.status(StatusCodes.BAD_REQUEST).render("pages/signup-form", {
+        actionPath: "/signup",
+        inputValues: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          confirmPassword: req.body.confirmPassword,
+        },
+        validationErrors,
+      });
+    }
+
+    return next();
+  } catch (error) {
+    return next(error);
+  }
 };
 
 export { signupValidationMiddleware };
