e12f9d27d0d65948eea123e1c5668e5887aefbf8
mojo <igormalj@gmail.com>
2024-12-04 16:26:09 +0100

    add supabase stroage config

--- src/controllers/fileController.ts
+++ src/controllers/fileController.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { StatusCodes } from "http-status-codes";
+import supabase from "../config/supabase/supabaseConfig.js";
 
 const uploadFileGet = (req: Request, res: Response, next: NextFunction) => {
   const { folderName } = req.params;
@@ -21,6 +22,16 @@ const uploadFilePost = async (
     const { folderName } = req.params;
     const {} = req.body;
     const file = req.file;
+
+    if (file) {
+      const { data, error } = await supabase.storage
+        .from("file")
+        .upload("new-file", file.buffer, {
+          cacheControl: "3600",
+          upsert: false,
+        });
+      console.log(data, error);
+    }
 
     console.log(file);
 
